9b6d39d615c90367de3e2583be1784b809f87881
posad97 <35921477+posad97@users.noreply.github.com>
2024-12-30 21:52:23 +0100

    Initial commit

---
+++ remove_images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while IFS= read -r line; 
+    do
+        image_name=$(echo $line | awk '{print $1}')
+        image_tag=$(echo $line | awk '{print $2}')
+        image_id=$(echo $line | awk '{print $3}')
+
+        if [[ "${image_name}" == "posad97/trading-app" ]]
+            then
+                docker rmi "${image_id}"
+        fi
+
+    done < <( docker image ls | tail -n +2)
+
+docker system prune -a --volumes -f
